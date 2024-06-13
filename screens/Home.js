0a2a42e8b9893c637744057af00339524a488a66
Midou <163797874+Midou-Mni@users.noreply.github.com>
2024-06-13 01:42:40 +0100

    Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,65 +1,111 @@
-import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
+import React from "react";
 import { globalStyles } from "../styles/global";
+import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
 
-import { Ionicons } from "@expo/vector-icons";
-import { FontAwesome6 } from "@expo/vector-icons";
-import { FontAwesome5 } from "@expo/vector-icons";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { FontAwesome } from "@expo/vector-icons";
+import {
+  Ionicons,
+  FontAwesome6,
+  FontAwesome5,
+  MaterialCommunityIcons,
+  FontAwesome,
+} from "@expo/vector-icons";
+
+import { useNavigation } from "@react-navigation/native";
 
 const logoImg = require("../images/logo.png");
 
 function Home() {
+  const navigation = useNavigation();
+
   return (
-    <ScrollView >
+    <ScrollView style={{ backgroundColor: "#e8e8e8" }}>
       <View style={globalStyles.container}>
         <Image source={logoImg} style={globalStyles.img} />
-
         <View style={globalStyles.cardContainer}>
-          <TouchableOpacity style={globalStyles.card}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Discharge")}
+            style={globalStyles.card}
+          >
             <FontAwesome name="file-text" size={22} color="white" />
             <Text style={globalStyles.cardTitle}>DISCHARGE</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={globalStyles.card}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Timetable")}
+            style={globalStyles.card}
+          >
             <Ionicons name="calendar" size={22} color="white" />
             <Text style={globalStyles.cardTitle}>Timetable</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={globalStyles.card}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("GroupAndSection")}
+            style={globalStyles.card}
+          >
             <FontAwesome6 name="user-group" size={22} color="white" />
             <Text style={globalStyles.cardTitle}>Group and section</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={globalStyles.card}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ExamSchedule")}
+            style={globalStyles.card}
+          >
             <Ionicons name="calendar" size={22} color="white" />
             <Text style={globalStyles.cardTitle}>Examen Schedule</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={globalStyles.card}>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ExamenGrade")}
+            style={globalStyles.card}
+          >
             <FontAwesome5 name="graduation-cap" size={22} color="white" />
             <Text style={globalStyles.cardTitle}>Examen Grade</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={globalStyles.card}>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Assessment")}
+            style={globalStyles.card}
+          >
             <Ionicons name="pencil-outline" size={22} color="white" />
             <Text style={globalStyles.cardTitle}>Assessment</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={globalStyles.card}>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Pourcentage")}
+            style={globalStyles.card}
+          >
             <Ionicons name="pie-chart" size={22} color="white" />
             <Text style={globalStyles.cardTitle}>Pourcentage (%)</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={globalStyles.card}>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate("AcademicTranscript")}
+            style={globalStyles.card}
+          >
             <Ionicons name="folder-open" size={22} color="white" />
             <Text style={globalStyles.cardTitle}>Academic transcript</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={globalStyles.card}>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Debts")}
+            style={globalStyles.card}
+          >
             <Ionicons name="calculator" size={22} color="white" />
             <Text style={globalStyles.cardTitle}>Debts</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={globalStyles.card}>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate("AcademicVacation")}
+            style={globalStyles.card}
+          >
             <FontAwesome name="file" size={22} color="white" />
             <Text style={globalStyles.cardTitle}>Academic vacation</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={globalStyles.card}>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Enrollment")}
+            style={globalStyles.card}
+          >
             <MaterialCommunityIcons name="inbox-full" size={22} color="white" />
             <Text style={globalStyles.cardTitle}>Enrollments</Text>
           </TouchableOpacity>
