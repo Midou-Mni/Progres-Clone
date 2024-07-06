8d75e1db9714838e6a1d03a112340139080ad7ba
Midou <163797874+Midou-Mni@users.noreply.github.com>
2024-07-06 23:38:17 +0100

    Update ExamenGrade.js

--- screens/homeScreens/ExamenGrade.js
+++ screens/homeScreens/ExamenGrade.js
@@ -1,79 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import { style } from "./StyleHomeScreens/style";
 
-const ExamenGrade = () => (
-  <ScrollView style={{ backgroundColor: "#e8e8e8" }}>
-    <View style={style.containerSemestre}>
-      <View style={style.semestreGroup}>
-        <TouchableOpacity style={style.touchGroup}>
-          <Text style={style.groupT}>semestre 1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={style.touchGroup}>
-          <Text style={style.groupT}>semestre 2</Text>
-        </TouchableOpacity>
-      </View>
+function ExamenGrade() {
+  const [examModule1, setExamModule1] = useState("");
+  const [examModule2, setExamModule2] = useState("");
+  const [examModule3, setExamModule3] = useState("");
+  const [examModule4, setExamModule4] = useState("");
+  const [examModule5, setExamModule5] = useState("");
 
-      <View style={style.groupCard}>
-        <View style={style.gradeRow}>
-          <Text style={style.gradeText1}>Module</Text>
-          <Text style={style.gradeText2}>Coef</Text>
-          <Text style={style.gradeText3}>Grade</Text>
-        </View>
-        
-        <View style={style.gradeRow}>
-          <Text style={style.gradeText1}>module Name 1:</Text>
-          <Text style={style.gradeText2}>/</Text>
-          <Text style={style.gradeText3}>/</Text>
-        </View>
-        
-        
-        <View style={style.gradeRow}>
-          <Text style={style.gradeText1}>module Name 2:</Text>
-          <Text style={style.gradeText2}>/</Text>
-          <Text style={style.gradeText3}>/</Text>
-        </View>
-        
-        
-        <View style={style.gradeRow}>
-          <Text style={style.gradeText1}>module Name 3:</Text>
-          <Text style={style.gradeText2}>/</Text>
-          <Text style={style.gradeText3}>/</Text>
-        </View>
-        
-        
-        <View style={style.gradeRow}>
-          <Text style={style.gradeText1}>module Name 4:</Text>
-          <Text style={style.gradeText2}>/</Text>
-          <Text style={style.gradeText3}>/</Text>
-        </View>
-        
-        
-        <View style={style.gradeRow}>
-          <Text style={style.gradeText1}>module Name 5:</Text>
-          <Text style={style.gradeText2}>/</Text>
-          <Text style={style.gradeText3}>/</Text>
-        </View>
-        
-        
-        <View style={style.gradeRow}>
-          <Text style={style.gradeText1}>module Name 6:</Text>
-          <Text style={style.gradeText2}>/</Text>
-          <Text style={style.gradeText3}>/</Text>
-        </View>
-        
-        
-        <View style={style.gradeRow}>
-          <Text style={style.gradeText1}>module Name 7:</Text>
-          <Text style={style.gradeText2}>/</Text>
-          <Text style={style.gradeText3}>/</Text>
-        </View>
-        
-        
+  useEffect(() => {
+    const fetchExam = async () => {
+      const response = await fetch("http://192.168.1.2:5000/get-assessment");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setExamModule1(data.examModule1);
+      setExamModule2(data.examModule2);
+      setExamModule3(data.examModule3);
+      setExamModule4(data.examModule4);
+      setExamModule5(data.examModule5);
+    };
 
+    fetchExam();
+  }, []);
+
+  const getColor = (a) => {
+    return a < 10 ? "red" : "green";
+  };
+
+  return (
+    <ScrollView style={{ backgroundColor: "#e8e8e8" }}>
+      <View style={style.containerSemestre}>
+        <View style={style.semestreGroup}>
+          <TouchableOpacity style={style.touchGroup}>
+            <Text style={style.groupT}>semestre 1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={style.touchGroup}>
+            <Text style={style.groupT}>semestre 2</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={style.groupCard}>
+          <View style={style.gradeRow}>
+            <Text style={style.gradeText1}>Module</Text>
+            <Text style={style.gradeText2}>Coef</Text>
+            <Text style={[style.gradeText3, { color: 'green' }]}>Grade</Text>
+          </View>
+
+          <View style={style.gradeRow}>
+            <Text style={style.gradeText1}>module Name 1:</Text>
+            <Text style={style.gradeText2}>/</Text>
+            <Text style={[style.gradeText3, { color: getColor(examModule1) }]}>{examModule1}</Text>
+          </View>
+
+          <View style={style.gradeRow}>
+            <Text style={style.gradeText1}>module Name 2:</Text>
+            <Text style={style.gradeText2}>/</Text>
+            <Text style={[style.gradeText3, { color: getColor(examModule2) }]}>{examModule2}</Text>
+          </View>
+
+          <View style={style.gradeRow}>
+            <Text style={style.gradeText1}>module Name 3:</Text>
+            <Text style={style.gradeText2}>/</Text>
+            <Text style={[style.gradeText3, { color: getColor(examModule3) }]}>{examModule3}</Text>
+          </View>
+
+          <View style={style.gradeRow}>
+            <Text style={style.gradeText1}>module Name 4:</Text>
+            <Text style={style.gradeText2}>/</Text>
+            <Text style={[style.gradeText3, { color: getColor(examModule4) }]}>{examModule4}</Text>
+          </View>
+
+          <View style={style.gradeRow}>
+            <Text style={style.gradeText1}>module Name 5:</Text>
+            <Text style={style.gradeText2}>/</Text>
+            <Text style={[style.gradeText3, { color: getColor(examModule5) }]}>{examModule5}</Text>
+          </View>
+        </View>
       </View>
-    </View>
-  </ScrollView>
-);
+    </ScrollView>
+  );
+}
 
 export default ExamenGrade;
