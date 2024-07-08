0c5861047cc1740ba5fd9a28b8ade077d02cf2c7
Midou <163797874+Midou-Mni@users.noreply.github.com>
2024-07-09 00:16:56 +0100

    Update App.js

--- admins/src/App.js
+++ admins/src/App.js
@@ -1,89 +1,26 @@
 import React, { useState } from "react";
 import "./App.css";
 
-import File from "./component/file";
+import Navbar from "./component/navbar";
+import NewStudent from "./component/studentComponent/NewStudent";
+import Majors from "./component/studentComponent/Majors";
 
 function App() {
-  const [name, setName] = useState("");
-  const [university, setUniversity] = useState("");
-  const [placeOfBirth, setPlaceOfBirth] = useState("");
-  const [dateOfBirth, setDateOfBirth] = useState("");
-  
-  const [Submitted, setSubmitted] = useState(false);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const response = await fetch("http://localhost:5000/send-name", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, university, placeOfBirth, dateOfBirth }),
-    });
-    const data = await response.json();
-    if (data.success) {
-      console.log("sent successfully");
-      console.log(data);
-      setSubmitted(true);
-    }
-  };
+  const [showAddStudent, setShowAddStudent] = useState(false);
+  const [showMajors, setShowMajors] = useState(false);
 
   return (
-    <div>
-      {Submitted ? (
-        <File />
-      ) : (
-        <div className="App">
-          <form className="form" onSubmit={handleSubmit}>
-            <section>
-              <label>
-                <p>Name: </p>
-                <input
-                  required
-                  type="text"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  placeholder="Enter student name"
-                />
-              </label>
-              <label>
-                <p>University: </p>
-                <input
-                  required
-                  type="text"
-                  value={university}
-                  onChange={(e) => setUniversity(e.target.value)}
-                  placeholder="Enter the university"
-                />
-              </label>
-            </section>
-
-            <section>
-              <label>
-                <p>Place of Birth: </p>
-                <input
-                  required
-                  type="text"
-                  value={placeOfBirth}
-                  onChange={(e) => setPlaceOfBirth(e.target.value)}
-                  placeholder="Place of Birth"
-                />
-              </label>
-              <label>
-                <p>Date of Birth: </p>
-                <input
-                  required
-                  type="date"
-                  value={dateOfBirth}
-                  onChange={(e) => setDateOfBirth(e.target.value)}
-                  placeholder="Date of Birth"
-                />
-              </label>
-            </section>
-            <button type="submit">Send</button>
-          </form>
-        </div>
-      )}
+    <div className="ALL">
+      <Navbar
+        showAddStudent={showAddStudent}
+        setShowAddStudent={setShowAddStudent}
+        showMajors={showMajors}
+        setShowMajors={setShowMajors}
+      />
+      <div className="w-full flex justify-center pb-[10vh]">
+        {showAddStudent && <NewStudent />}
+        {showMajors && <Majors />}
+      </div>
     </div>
   );
 }
